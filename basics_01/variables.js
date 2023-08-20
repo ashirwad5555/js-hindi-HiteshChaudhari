db62215f9e3765526164a68ebf4860499725f4dc
Ashirwad Rajeshwar Katkamwar <128044412+ashirwad5555@users.noreply.github.com>
2023-08-20 10:51:50 +0000

    variables

---
+++ basics_01/variables.js
@@ -0,0 +1,9 @@
+const accId = 5555
+let accName = "ashirwad"
+var accCity = "pune"
+
+accBrand = "ARK🔥"
+
+console.log("see these all variables in tabular form ")
+
+console.table([accId , accCity,accName,accBrand])
