c322744dddef5719d3dec473ea3e1ddf2974d9d9
Ashirwad Rajeshwar Katkamwar <128044412+ashirwad5555@users.noreply.github.com>
2023-08-28 18:23:48 +0000

    datatype

---
+++ basics_01/datatypes.js
@@ -0,0 +1,18 @@
+"use strict";    //treat all js code as new version 
+
+let name = "Hitesh"
+let age = 19
+let isLogged = true
+let state;
+// myVar = 42;  //this type of global declaration is not allowed in strict mode of js 
+
+
+console.table([name, age, isLogged, state]);
+
+console.log(typeof "ashirwad")
+console.log(typeof name);
+console.log(typeof age)
+console.log(typeof undefined)
+console.log(typeof null)
+console.log(typeof myVar)
+ 
