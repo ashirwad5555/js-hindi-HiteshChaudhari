f51568294973ee82f685fea682145f2b352f47e6
Ashirwad Rajeshwar Katkamwar <128044412+ashirwad5555@users.noreply.github.com>
2023-12-14 13:56:41 +0000

    datatype conversion

--- basics_01/datatypes.js
+++ basics_01/datatypes.js
@@ -1,18 +1,51 @@
 "use strict";    //treat all js code as new version 
 
-let name = "Hitesh"
-let age = 19
-let isLogged = true
-let state;
-// myVar = 42;  //this type of global declaration is not allowed in strict mode of js 
+// let name = "Hitesh"
+// let age = 19
+// let isLogged = true
+// let state;
+// // myVar = 42;  //this type of global declaration is not allowed in strict mode of js 
 
 
-console.table([name, age, isLogged, state]);
+// console.table([name, age, isLogged, state]);
 
-console.log(typeof "ashirwad")
-console.log(typeof name);
-console.log(typeof age)
-console.log(typeof undefined)
-console.log(typeof null)
-console.log(typeof myVar)
+// console.log(typeof "ashirwad")
+// console.log(typeof name);
+// console.log(typeof age)
+// console.log(typeof undefined)
+// console.log(typeof null)
+// console.log(typeof myVar)
+
+//string to number 
+
+// let score = "true";
+
+// let sgpa = Number(score);
+// console.log(sgpa);
+// console.log(typeof sgpa);
+
+// "abc123" => NaN .. not a number  NaN is also a number
+// "124"   => 124
+// "undefined" => NaN 
+
+
+//string to boolean 
+
+let variable = ""
+let ans = Boolean(variable);
+console.log(ans)
+console.log(typeof(ans));
+
+// "1" => true; "0" =>false
+// "" =>false  "ashirwad" => true
+
+// number to string 
+let num = 33;
+
+ ans = String(num);
+console.log(ans);
+console.table([ans, typeof ans])
+
+
+
  
